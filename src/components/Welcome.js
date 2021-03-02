2b278975e42bbb9dfc975dfe6b8f4f27e591500f
rahul9852-dot <66193581+rahul9852-dot@users.noreply.github.com>
2021-03-02 16:07:06 +0530

    error

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,30 @@
-// write code for Welcome component here
+// // write code for Welcome component here
+
+// import React from 'react';
+
+// function Welcome(props){
+//   return(
+//     <div>
+//       <h1>`$Hey {props.name}`</h1>
+//       <h2>Welcome to Newton School. How are you? How are you doing?</h2>
+//     </div>
+//   )
+// }
+
+
+
+// export default Welcome;
 
 import React from 'react';
+// import App from './App';
 
-function Welcome(props){
-  return(
-    <div>
-      <h1>`$Hey {props.name}`</h1>
-      <h2>Welcome to Newton School. How are you? How are you doing?</h2>
-    </div>
-  )
-}
-
-
+const Welcome = (props) => {
+    return (
+        <div>
+            <h1>Hey {props.name}!</h1>
+            <h2>Welcome to Newton School. How are you? How are you doing?</h2>
+        </div>
+    );
+};
 
 export default Welcome;
-
